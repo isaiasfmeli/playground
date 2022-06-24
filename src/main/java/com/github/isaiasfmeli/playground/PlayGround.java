da5df5d61a8ba1ee4fec36338e0c341a2cc74600
Isaias F <isaias.finger@mercadolivre.com>
2022-06-24 10:12:13 -0300

    feat: Prática Integradora 1

---
+++ src/main/java/com/github/isaiasfmeli/playground/PlayGround.java
@@ -0,0 +1,11 @@
+package com.github.isaiasfmeli.playground;
+
+import com.github.isaiasfmeli.playground.modulo2.TemperaturasGlobais;
+
+public class PlayGround {
+
+    public static void main(String[] args) {
+        System.out.println("- Módulo 2 / Prática Integradora 1 / Temperaturas Globais");
+        TemperaturasGlobais.main(args);
+    }
+}
---
+++ src/main/java/com/github/isaiasfmeli/playground/modulo2/TemperaturasGlobais.java
@@ -0,0 +1,29 @@
+package com.github.isaiasfmeli.playground.modulo2;
+
+public class TemperaturasGlobais {
+
+    public static final int COLUNA_TEMP_MIN = 0;
+    public static final int COLUNA_TEMP_MAX = 1;
+
+    public static void main(String[] args) {
+        String[] cidades = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int[][] temperaturas = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
+
+        String cidadeMin = "", cidadeMax = "";
+        int tempMin = 0, tempMax = 0;
+
+        for (int i = 0; i < temperaturas.length; i++) {
+            if (tempMin > temperaturas[i][COLUNA_TEMP_MIN]) {
+                tempMin = temperaturas[i][COLUNA_TEMP_MIN];
+                cidadeMin = cidades[i];
+            }
+            if (tempMax < temperaturas[i][COLUNA_TEMP_MAX]) {
+                tempMax = temperaturas[i][COLUNA_TEMP_MAX];
+                cidadeMax = cidades[i];
+            }
+        }
+
+        System.out.printf("A cidade de %s registrou a temperatura mais Baixa de %d°C\n", cidadeMin, tempMin);
+        System.out.printf("A cidade de %s registrou a temperatura mais Alta de %d°C\n", cidadeMax, tempMax);
+    }
+}
