aa9b6095590ec95e73c6f2b2b425eb19f22cdc81
Isaias <isaias.finger@mercadolivre.com>
2022-07-05 20:55:51 -0300

    feat: User with lombok

--- src/main/java/com/github/isaiasfmeli/playground/modulo3/model/User.java
+++ src/main/java/com/github/isaiasfmeli/playground/modulo3/model/User.java
@@ -1,33 +1,14 @@
 package com.github.isaiasfmeli.playground.modulo3.model;
 
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.boot.jackson.JsonComponent;
 
+@Data
 @JsonComponent
 public class User {
 
     int id;
     String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static User builder(String name)
-    {
-        User user = new User();
-        user.setName(name);
-        return user;
-    }
 }
